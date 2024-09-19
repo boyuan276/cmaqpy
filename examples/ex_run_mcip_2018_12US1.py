d7f15c19ef09c730841cdf11452728136d7f73e3
Bo Yuan <by276@cornell.edu>
2024-09-19 10:25:57 -0400

    add code for 2018 12US1

---
+++ examples/ex_run_mcip_2018_12US1.py
@@ -0,0 +1,54 @@
+"""
+This example shows how to process wrfout files with MCIP using the `CMAQModel` class.
+
+Note that MCIP will fail if your `start_datetime` is not AFTER the fist timestep of 
+your wrfout*.nc file.
+"""
+
+from cmaqpy.runcmaq import CMAQModel
+
+start_datetime = "July 9, 2018"  # first day that you want processed
+end_datetime = "July 10, 2018"  # ONE DAY AFTER the last day you want processed
+
+# Specify if you want to run the 12 km or the 4 km domain
+# appl = '2016_12OTC2'
+appl = "2018_12US1"
+
+coord_name = "LAM_40N97W"
+if appl == "2016_12OTC2":
+    grid_name = "12OTC2"
+elif appl == "2016_4OTC2":
+    grid_name = "4OTC2"
+elif appl == "2018_12US1":
+    grid_name = "12US1"
+
+# Create a CMAQModel object
+cmaq_sim = CMAQModel(
+    start_datetime,
+    end_datetime,
+    appl,
+    coord_name,
+    grid_name,
+    setup_yaml=f"dirpaths_{appl}.yml",
+    new_mcip=True,
+    new_icon=False,
+    new_bcon=False,
+    verbose=True,
+)
+
+# Specify the meteorolocial files
+if appl == "2016_12OTC2":
+    metfile_list = ["wrfout_d01_2016-08-05_00:00:00"]
+elif appl == "2016_4OTC2":
+    metfile_list = ["wrfout_d02_2016-08-05_00:00:00"]
+
+# Call the "run_mcip" method
+# TODO: Specify metfile_dir according to WRF output files
+if appl == "2016_12OTC2":
+    cmaq_sim.run_mcip_multiday(
+        metfile_dir=None, metfile_list=metfile_list, geo_file="geo_em.d01.nc", t_step=60
+    )
+elif appl == "2016_4OTC2":
+    cmaq_sim.run_mcip_multiday(
+        metfile_dir=None, metfile_list=metfile_list, geo_file="geo_em.d02.nc", t_step=60
+    )
