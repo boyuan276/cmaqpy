dc43def0913b2f8b4a2db07cc2187267b1ea11c9
Bo Yuan <by276@cornell.edu>
2024-10-02 00:16:05 -0400

    Refactor ex_run_combine_2018_12US1.py to update start and end datetime, application name, and emission labels

--- examples/ex_run_combine_2018_12US1.py
+++ examples/ex_run_combine_2018_12US1.py
@@ -2,30 +2,25 @@
 This example shows how to run the CCTM benchmark case using the `CMAQModel` class.
 
 You should run this inside a tmux window because this ties up the terminal.
+
+NOTE: Check MCIP input file names $YYYY$MM$DD or $YY$MM$DD in the run script.
 """
 
 from cmaqpy.runcmaq import CMAQModel
 
 # Specify the start/end times
-start_datetime = "August 06, 2016"  # first day that you want run
-end_datetime = "August 14, 2016"  # DAY AFTER the last day you want run
+start_datetime = "2018-07-02"  # first day that you want run
+end_datetime = "2018-07-09"  # DAY AFTER the last day you want run
 
 # Specify if you want to run the 12 km or the 4 km domain
-appl = "2016_12OTC2"
-# appl = '2016Base_12OTC2'
-# appl = '2016_4OTC2'
-# appl = '2016Base_4OTC2'
+appl = "2018_12US1"
 
 # Define the coordinate name (must match that in GRIDDESC)
-coord_name = "LAM_40N97W"
-if appl == "2016_12OTC2":
-    grid_name = "12OTC2"
-elif appl == "2016Base_12OTC2":
-    grid_name = "12OTC2"
-elif appl == "2016_4OTC2":
-    grid_name = "4OTC2"
-elif appl == "2016Base_4OTC2":
-    grid_name = "4OTC2"
+if "2018_12US1" in appl:
+    coord_name = "LAM_40N97W"
+    grid_name = "12US1"
+else:
+    raise ValueError(f"Unknown application: {appl}")
 
 # Create a CMAQModel object
 cmaq_sim = CMAQModel(
@@ -35,8 +30,11 @@ cmaq_sim = CMAQModel(
     coord_name,
     grid_name,
     setup_yaml=f"dirpaths_{appl}.yml",
+    new_mcip=False,
+    new_bcon=False,
+    new_icon=False,
     verbose=True,
 )
 
 # Call the "run_combine" method
-cmaq_sim.run_combine(run_hours=2, mem_per_node=20, combine_vrsn="v54")
+cmaq_sim.run_combine(run_hours=2, mem_per_node=20, combine_vrsn="v54", setup_only=True)
